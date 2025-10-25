fa97b88f8a11d8c7f8a437aff24fe3aac609ff7a
cgburnsi <117243547+cgburnsi@users.noreply.github.com>
2025-10-25 09:19:48 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,22 @@
+# utils/Quantity/__init__.py
+
+from .quantity import (
+    Quantity,
+    UnitDefinition,
+    UnitParser,
+    UNITS,
+    resolve_units,
+)
+
+# Expose the submodule itself if you ever want to import it directly
+from . import quantity as quantity
+
+__all__ = [
+    "Quantity",
+    "UnitDefinition",
+    "UnitParser",
+    "UNITS",
+    "resolve_units",
+    "quantity",
+]
 
